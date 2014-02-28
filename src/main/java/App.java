8cd85020275c6841f6a1af7e90f56c1e54883750
Paul Sabatino <paul.sabatino@gmail.com>
2014-02-27 21:42:16 -0500

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,74 @@
+import com.sabstuff.lambda.App.Address;
+import com.sabstuff.lambda.App.AddressForm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User: P.Sabatino (paul@qstartlabs.com)
+ * Date: 2/26/14
+ * Time: 9:59 PM
+ */
+public class App {
+
+
+    public static void main(String[] args) {
+
+        App a = new App();
+        long start = System.currentTimeMillis();
+        List<Address> list = generateAddressList(100000);
+        System.out.println("list generation = " + (System.currentTimeMillis() - start) + "ms");
+
+        start = System.currentTimeMillis();
+        List<AddressForm> forms = a.runParallel(list);
+        System.out.println("Parallel = " + (System.currentTimeMillis() - start) + "ms");
+        System.out.println("\tform list size: "+forms.size());
+
+        start = System.currentTimeMillis();
+        forms = a.runSerial(list);
+        System.out.println("Serial = "+(System.currentTimeMillis()-start)+"ms");
+        System.out.println("\tform list size: "+forms.size());
+
+        start = System.currentTimeMillis();
+        forms = a.runParallel(list);
+        System.out.println("Parallel2 = " + (System.currentTimeMillis() - start) + "ms");
+        System.out.println("\tform list size: "+forms.size());
+
+
+    }
+
+
+    public List<AddressForm> runSerial(List<Address> list) {
+
+        List<AddressForm> forms = new ArrayList<AddressForm>();
+        list.stream()
+                .forEach(p -> forms.add(populateForm(p)));
+
+        return forms;
+    }
+
+    public List<AddressForm> runParallel(List<Address> list) {
+
+        List<AddressForm> forms = new ArrayList<AddressForm>();
+        list.parallelStream()
+                .map(p -> populateForm(p)).map(p->forms.add(p));
+
+        return forms;
+    }
+
+
+    private AddressForm populateForm(Address a ) {
+        return new AddressForm();
+    }
+
+
+    private static List<Address> generateAddressList(int n) {
+
+        List<Address> list = new ArrayList<Address>();
+        for (int i = 0; i < n; i++) {
+            list.add(new Address());
+        }
+
+        return list;
+    }
+}
---
+++ src/main/java/com/sabstuff/lambda/App/AddressForm.java
@@ -0,0 +1,67 @@
+package com.sabstuff.lambda.App;
+
+/**
+ * User: P.Sabatino (paul@qstartlabs.com)
+ * Date: 2/26/14
+ * Time: 10:05 PM
+ */
+public class AddressForm {
+
+    private String address;
+    private String address2;
+    private String city;
+    private String state;
+    private String zip;
+    private String notes;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+
+
+}
